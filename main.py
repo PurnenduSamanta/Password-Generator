489fc5517cdf7bf1242e2566452cf3e1630e76fd
Kuntal Sarkar <kuntal.sarkar383@gmail.com>
2020-10-06 11:59:20 +0530

    Import p line removed and added some comment

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import p as p
 from itertools import permutations
+
 user_password = input("Enter your password: ")
+
+#removing bad chars
 bad_chars = [" "]
 for i in bad_chars:
     user_password = user_password.replace(i, '')
+
+#getting all the permutations of user entered input 
+#and storing it into a list
 for word in [''.join(p) for p in permutations(user_password)]:
     print(word)
